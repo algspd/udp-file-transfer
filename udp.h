1da60017ec0be5691b1aaeec8a6d4f49884895cd
Javier Briz <algspd@gmail.com>
2010-11-11 23:43:38 +0100

    Added udp.h

---
+++ udp.h
@@ -0,0 +1,6 @@
+
+#include "udp.c"
+
+extern int send_buf(int sock,struct sockaddr_in *server,char *buf,int buflen);
+extern int start_server(int port);
+extern int receive(int s,char *rbuffer,int rbufferlen);
