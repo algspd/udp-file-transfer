0b53b46d00d05fb91846716e01b2e7ce605a413e
Javier Briz <algspd@gmail.com>
2010-11-12 18:49:58 +0100

    Stuffing with MD5, designing protocol

--- sender.c
+++ sender.c
@@ -22,18 +22,63 @@
 
 #include "udp.h"
 
+#include <openssl/evp.h>
+
+
 #define ERRORSTRING "unknown error"
 #define MAXNAMELEN 256
 
 int send_buf();
 
+char hexToAscii(char in)
+{
+  int cero='0';
+  int laa='a';
+  char foo;
+  if (in<10){
+    return(in+cero);
+  }
+  return(in+laa);
+}
+
 int main (int argc,char **argv){
    struct sockaddr_in server;
    struct hostent *host_info;
    char SERVER[MAXNAMELEN],*server1;
-   int sock,SERVER_PORT;
+   int sock,SERVER_PORT,i;
    char usage[100]="port@host\n\0";
    char buf[10];
+   EVP_MD_CTX mdctx;
+   
+   unsigned char foo[100];
+   char *md;
+   long n=1;
+   strcpy(foo,"h");
+
+      printf("long vale %li\n",n);
+      printf("foo vale %s\n\n",foo);
+      
+      
+//   static void calculate_md5_of(const void *content, ssize_t len){
+
+  unsigned char md_value[EVP_MAX_MD_SIZE];
+  unsigned int md_len;
+  
+  EVP_DigestInit(&mdctx, EVP_md5());
+  EVP_DigestUpdate(&mdctx, foo, (size_t) 1);
+  EVP_DigestFinal_ex(&mdctx, md_value, &md_len);
+  EVP_MD_CTX_cleanup(&mdctx);
+
+
+//    md=MD5(foo,n,NULL);
+
+  for(i = 0; i < md_len; i++){
+    printf ("%02x", md_value[i]);
+  }
+
+   
+//    printf("md vale %d %d %d \n\n",md[0],md[1],md[2],md[3]);
+   exit (2);
 
    /* PARSE COMMAND LINE ARGUMENTS */
    if (argc<2){
