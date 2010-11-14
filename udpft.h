55417c3317040fd606b7cb67ec16ff75a27c2b60
Javier Briz <algspd@gmail.com>
2010-11-14 13:24:56 +0100

    Updated packet definition

--- udpft.h
+++ udpft.h
@@ -15,16 +15,51 @@
    along with "UDP file transfer".  If not, see <http://www.gnu.org/licenses/>.
 */
 
-typedef char t_type[1];
-typedef char md5[33];
+#include <stdio.h>
+#include <stdlib.h>
+#define FRAGMENT_SIZE 1000
+
+typedef char typ_t[1];
+typedef char md5_t[33];
+typedef int  fid_t;
+typedef int  fsi_t;
+typedef int  fex_t;
+typedef char fra_t[FRAGMENT_SIZE];
 
 /* CLIENT */
 struct fgetinfo{
-   t_type type;
-   char file_path[FILENAME_MAX];
-   t_md5 md5;
-}
+   typ_t type;
+   char  file_path[FILENAME_MAX];
+   md5_t md5;
+};
 
+struct fgetfrag{
+   typ_t type;
+   fid_t file_id;
+   off_t offset;
+   md5_t md5;
+};
 
+struct fend{
+   typ_t type;
+   fid_t file_id;
+   md5_t md5;
+};
 
 /* SERVER */
+
+struct finfo{
+   typ_t type;
+   fex_t file_exist;
+   fid_t file_id;
+   fsi_t file_size;
+   md5_t md5;
+};
+
+struct ffrag{
+   typ_t type;
+   fid_t file_id;
+   off_t offset;
+   fra_t fragment;
+   md5_t md5;
+};
